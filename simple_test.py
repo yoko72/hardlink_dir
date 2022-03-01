ce7fffb6a68e33924cb391cbe03835de48c4d5db
owner <yokoyamacode@gmail.com>
2022-03-01 16:16:13 +0900

    fixed path

--- simple_test.py
+++ simple_test.py
@@ -2,7 +2,7 @@ from os import sep, getcwd
 from sys import argv
 from pathlib import Path
 
-print(getcwd())
-argv.append("C:\\Users\\owner\\Desktop\\test")
+YOUR_TEST_SOURCE = "C:\\Users\\owner\\Desktop\\test"  # replace this to test
+argv.append(YOUR_TEST_SOURCE)
 
 from hardlink_dir import __main__
