73954b152e55a5969213a09d73a5d4dfe1350723
owner <yokoyamacode@gmail.com>
2022-03-02 08:49:31 +0900

    deleted unused imports

--- simple_test.py
+++ simple_test.py
@@ -1,6 +1,4 @@
-from os import sep, getcwd
 from sys import argv
-from pathlib import Path
 
 YOUR_TEST_SOURCE = "C:\\Users\\owner\\Desktop\\test"  # replace this to test
 argv.append(YOUR_TEST_SOURCE)
